458fda192f800d542bc2d910b6fd9f3fbfa2fdae
Bünyamin Kardeş <bunyaminkardes@outlook.com>
2023-04-24 16:20:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require("express")
 const PORT = 3000;
 
 
+const express = require("express");
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => {
     res.send("ilk");
